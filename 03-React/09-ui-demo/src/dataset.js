cf564306a1ba81d2de85ef73c0a9cf70ec324392
pongpongie <arevolvera@gmail.com>
2025-02-15 17:14:38 +0900

    [Feat] 09-ui-demo - mod for step4

--- 03-React/09-ui-demo/src/dataset.js
+++ 03-React/09-ui-demo/src/dataset.js
@@ -1,52 +1,109 @@
-const tabContent = [
-  {
-    id: "newyork",
-    subject: "NewYork",
-    content: "NewYork is the capital city of US.",
-  },
-  {
-    id: "london",
-    subject: "London",
-    content: "London is the capital city of England.",
-  },
-  {
-    id: "paris",
-    subject: "Paris",
-    content: "Paris is the capital city of France.",
-  },
-  {
-    id: "seoul",
-    subject: "Seoul",
-    content: "Seoul is the capital city of Korea.",
-  },
-];
+const tabContent = [{
+    id: 'newyork',
+    subject: 'NewYork',
+    content: 'NewYork is the capital city of US.'
+}, {
+    id: 'london',
+    subject: 'London',
+    content: 'London is the capital city of England.'
+}, {
+    id: 'paris',
+    subject: 'Paris',
+    content: 'Paris is the capital city of France.'
+}, {
+    id: 'seoul',
+    subject: 'Seoul',
+    content: 'Seoul is the capital city of Korea.'
+}];
 
-const collapseContent = [
-  {
-    title: "Open Collapsible",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.",
-  },
-  {
-    title: "Open Collapsible",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.",
-  },
-  {
-    title: "Open Collapsible",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.",
-  },
-  {
-    title: "Open Collapsible",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.",
-  },
-  {
-    title: "Open Collapsible",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.",
-  },
-];
+const menuData = [{
+    id: 1,
+    label: "FrontEnd",
+    url: "#",
+    children: [{
+        id: 4,
+        label: "HTML+CSS",
+        url: "#"
+    }, {
+        id: 5,
+        label: "Javascript",
+        url: "#"
+    }, {
+        id: 6,
+        label: "React.js",
+        url: "#"
+    }]
+}, {
+    id: 2,
+    label: "Backend",
+    url: "#",
+    children: [{
+        id: 7,
+        label: "Spring (Java)",
+        url: "#"
+    }, {
+        id: 8,
+        label: "Laravel (PHP)",
+        url: "#"
+    }, {
+        id: 9,
+        label: "Express (Node.js)",
+        url: "#"
+    }, {
+        id: 10,
+        label: "Django (Python)",
+        url: "#",
+    }, {
+        id: 11,
+        label: "Flask (Python)",
+        url: "#",
+    }, {
+        id: 12,
+        label: "Fast API (Python)",
+        url: "#",
+    }]
+}, {
+    id: 3,
+    label: "Mobile",
+    url: "#",
+    children: [{
+        id: 13,
+        label: "Swift (iOS)",
+        url: "#",
+    }, {
+        id: 14,
+        label: "Java (Android)",
+        url: "#",
+    }, {
+        id: 15,
+        label: "Kotlin (Android)",
+        url: "#",
+    }, {
+        id: 16,
+        label: "React Native (Hybrid)",
+        url: "#",
+    }, {
+        id: 17,
+        label: "Fluter (Hybrid)",
+        url: "#",
+    }]
+}];
 
-export { tabContent, collapseContent };
+const collapseContent = [{
+    title: 'Open Collapsible',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.'
+}, {
+    title: 'Open Collapsible',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.'
+}, {
+    title: 'Open Collapsible',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.'
+}, {
+    title: 'Open Collapsible',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.'
+}, {
+    title: 'Open Collapsible',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex eacommodo consequat.'
+}];
+
+export { tabContent, menuData, collapseContent };
--- 03-React/09-ui-demo/src/pages/submenu_ex/index.js
+++ 03-React/09-ui-demo/src/pages/submenu_ex/index.js
@@ -1,15 +1,99 @@
-import React, { memo } from "react";
+import React, { memo, useCallback } from "react";
+import { Link } from "react-router-dom";
 
 import styled from "styled-components";
 
+import { menuData } from "../../dataset";
+
+import btn from "../../assets/img/btn.png";
+import btnOver from "../../assets/img/btn_over.png";
+
 const SubmenuExContainer = styled.div`
-  background-color: #ff000055;
+  .menu-container {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+
+    .menu-item {
+      position: relative;
+
+      .sub {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+
+        height: 0;
+        overflow: hidden;
+        transition: height 180ms ease-out;
+
+        position: absolute;
+        z-index: 99999;
+        left: 0px;
+        top: 48px;
+      }
+    }
+    .link {
+      background: url(${btn});
+      display: flex;
+      width: 179px;
+      height: 48px;
+      align-items: center;
+      justify-content: center;
+      font-weight: bold;
+      color: #cfdfb5;
+      text-decoration: none;
+
+      &:hover {
+        background: url(${btnOver});
+      }
+    }
+  }
 `;
 
 const SubmenuEx = memo(() => {
+  const onMenuItemOver = useCallback((e) => {
+    const current = e.currentTarget;
+    const sub = current.querySelector(".sub");
+    sub.style.height = `${sub.scrollHeight}px`;
+  }, []);
+
+  const onMenuItemOut = useCallback((e) => {
+    e.currentTarget.querySelector(".sub").style.height = "0px";
+  }, []);
   return (
     <SubmenuExContainer>
       <h2>SubmenuEx</h2>
+      <ul className="menu-container">
+        {menuData.map((v, i) => {
+          return (
+            <li
+              key={v.id}
+              className="menu-item"
+              onMouseOver={onMenuItemOver}
+              onMouseOut={onMenuItemOut}
+            >
+              <Link to={v.url} className="link">
+                {v.label}
+              </Link>
+
+              {v.children && (
+                <ul className="sub">
+                  {" "}
+                  {v.children.map((v2, i2) => {
+                    return (
+                      <li key={v2.id} className="link">
+                        {v2.label}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+      <h2>안녕하세요 리액트</h2>
     </SubmenuExContainer>
   );
 });

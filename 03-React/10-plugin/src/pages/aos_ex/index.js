37eefb9b887664bf3abe8f8e24e2d8f8522464e2
pongpongie <arevolvera@gmail.com>
2025-02-16 15:30:55 +0900

    [Feat] 10-plugin - mod AosEx

--- 03-React/10-plugin/src/pages/aos_ex/index.js
+++ 03-React/10-plugin/src/pages/aos_ex/index.js
@@ -1,15 +1,70 @@
-import React, { memo } from "react";
+import React, { memo, useEffect } from "react";
 
 import styled from "styled-components";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const AosExContainer = styled.div`
-  background-color: #ff000055;
+  .box {
+    width: 300px;
+    height: 200px;
+    font-size: 40px;
+    color: white;
+    background-color: #f60;
+    text-align: center;
+    line-height: 200px;
+    margin: 250px auto;
+  }
 `;
 
 const AosEx = memo(() => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <AosExContainer>
       <h2>AosEx</h2>
+
+      <div
+        className="box"
+        data-aos="fade-zoom-in"
+        data-aos-anchor-placement="top-center"
+        data-aos-duration="600"
+        data-aos-easing="ease-in-sine"
+      >
+        AOS
+      </div>
+      <div
+        className="box"
+        data-aos="fade-left"
+        data-aos-anchor-placement="top-center"
+        data-aos-duration="600"
+        data-aos-easing="ease-in-sine"
+      >
+        AOS
+      </div>
+      <div
+        className="box"
+        data-aos="fade-right"
+        data-aos-anchor-placement="top-center"
+        data-aos-duration="600"
+        data-aos-easing="ease-in-sine"
+      >
+        AOS
+      </div>
+
+      <div id="example-anchor1"> example anchor1</div>
+
+      <div
+        className="box"
+        data-aos="fade-down"
+        data-aos-anchor="#example-anchor1"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="600"
+      >
+        AOS
+      </div>
     </AosExContainer>
   );
 });

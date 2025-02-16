db5bb10aef83e31e164adc7739e9236b2ecd4223
pongpongie <arevolvera@gmail.com>
2025-02-16 15:45:03 +0900

    [Feat] 10-plugin - mod SliderEx

--- 03-React/10-plugin/src/pages/slider_ex/index.js
+++ 03-React/10-plugin/src/pages/slider_ex/index.js
@@ -2,15 +2,35 @@ import React, { memo } from "react";
 
 import styled from "styled-components";
 
-const SliderExContainer = styled.div`
-  background-color: #ff000055;
-`;
+import ImageSlider from "react-simple-image-slider";
+import slide1 from "../../assets/img/slide1.jpg";
+import slide2 from "../../assets/img/slide2.jpg";
+import slide3 from "../../assets/img/slide3.jpg";
+import slide4 from "../../assets/img/slide4.jpg";
 
 const SliderEx = memo(() => {
+  const images = [
+    { url: slide1 },
+    { url: slide2 },
+    { url: slide3 },
+    { url: slide4 },
+  ];
+
   return (
-    <SliderExContainer>
+    <>
       <h2>SliderEx</h2>
-    </SliderExContainer>
+      <ImageSlider
+        width="100%"
+        height={480}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+        autoPlay={true}
+        autoPlayDelay={2.0}
+        loop={true}
+        style={{ marginLeft: "-50px" }}
+      />
+    </>
   );
 });
 

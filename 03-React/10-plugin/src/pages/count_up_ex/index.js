5dd2f2ee670e04ad510d77031a193e5c7b88c7f9
pongpongie <arevolvera@gmail.com>
2025-02-16 15:36:32 +0900

    [Feat] 10-plugin - mod CountUpEx

--- 03-React/10-plugin/src/pages/count_up_ex/index.js
+++ 03-React/10-plugin/src/pages/count_up_ex/index.js
@@ -2,14 +2,65 @@ import React, { memo } from "react";
 
 import styled from "styled-components";
 
+import CountUp from "react-countup";
+
 const CountUpExContainer = styled.div`
-  background-color: #ff000055;
+  .counter-box {
+    display: flex;
+    padding: 100px 50px;
+    justify-content: space-around;
+
+    .my-counter {
+      font-size: 80px;
+      font-weight: bold;
+      width: 300px;
+      height: 250px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #555;
+      color: #fff;
+    }
+  }
 `;
 
 const CountUpEx = memo(() => {
   return (
     <CountUpExContainer>
       <h2>CountUpEx</h2>
+
+      <hr />
+      <div
+        style={{
+          height: "3000px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "start",
+          fontSize: "32px",
+        }}
+      >
+        아래로 스크롤
+      </div>
+      <hr />
+
+      <div className="counter-box">
+        <CountUp
+          start={1}
+          end={345}
+          duration={5}
+          enableScrollSpy
+          scrollSpyDelay={1000}
+          className="my-counter"
+        />
+        <CountUp
+          start={1}
+          end={234}
+          duration={5}
+          enableScrollSpy
+          scrollSpyDelay={500}
+          className="my-counter"
+        />
+      </div>
     </CountUpExContainer>
   );
 });

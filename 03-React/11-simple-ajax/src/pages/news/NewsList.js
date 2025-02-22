4eaac310128f205b94d605533917c5eae6f0ddcd
pongpongie <arevolvera@gmail.com>
2025-02-22 15:41:16 +0900

    [Feat] 11-simple-ajax - step2

--- 03-React/11-simple-ajax/src/pages/news/NewsList.js
+++ 03-React/11-simple-ajax/src/pages/news/NewsList.js
@@ -1,7 +1,129 @@
 import React from "react";
+import styled from "styled-components";
 
-const NewsList = () => {
-  return <div></div>;
+const NewsListContainer = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+
+  .news-item {
+    border-top: 1px solid #eee;
+
+    &:last-child {
+      border-bottom: 1px solid #eee;
+    }
+
+    .list-item-link {
+      box-sizing: border-box;
+      display: flex;
+      align-items: center;
+      padding: 5px 10px;
+      text-decoration: none;
+      color: #000;
+      transition: all 0.1s;
+
+      &.hover {
+        background-color: #eeeeee55;
+      }
+
+      .thumbnail {
+        width: 150px;
+        height: 150px;
+        height: 100px;
+        display: block;
+        object-fit: cover;
+      }
+
+      .content {
+        flex: 0 1 auto;
+        padding: 5px 0 6px 20px;
+        background-color: #ff01;
+
+        h3 {
+          /* background-color: #f0f; */
+          box-sizing: border-box;
+          font-size: 18px;
+          font-weight: bold;
+          margin: 0;
+          margin-bottom: 10px;
+
+          display: -webkit-box;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+        }
+
+        p {
+          /* background-color: #0601; */
+          font-size: 14px;
+          margin: 0;
+          margin-bottom: 8px;
+          display: -webkit-box;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+        }
+
+        ul {
+          /* background-color: #0061; */
+          list-style: none;
+          padding: 0;
+          margin: 0;
+
+          li {
+            display: inline-block;
+            font-size: 12px;
+
+            &:first-child:after {
+              content: "|";
+              display: inline-block;
+              color: #555;
+              padding: 0 5px;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const NewsList = ({ news }) => {
+  console.group("NewsList");
+  console.log(news);
+  console.groupEnd();
+
+  return (
+    <NewsListContainer>
+      {news.map((v, i) => {
+        const { url, image, title, description, author, datetime } = v;
+
+        return (
+          <li className="news-item" key={i}>
+            <a className="list-item-link" href={url} target="_blank">
+              <img className="thumbnail" src={image} alt={title} />
+              <div className="content">
+                <h3 className="content">
+                  <h3>{title}</h3>
+                  <p>{description}</p>
+                  <ul>
+                    <li>{author}</li>
+                    <li>{new Date(datetime).toLocaleString()}</li>
+                  </ul>
+                </h3>
+              </div>
+            </a>
+          </li>
+        );
+      })}
+    </NewsListContainer>
+  );
 };
 
 export default NewsList;
--- 03-React/11-simple-ajax/src/pages/news/index.js
+++ 03-React/11-simple-ajax/src/pages/news/index.js
@@ -1,20 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Routes, Route } from "react-router-dom";
 import NewsCard from "./NewsCard";
 import NewsList from "./NewsList";
 
+import axiosHelper from "../../helpers/AxiosHelper";
+
 const News = () => {
   const [newsData, setNewsData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let data = null;
+
+      try {
+        data = await axiosHelper.get("/news");
+      } catch (e) {
+        alert(e.message);
+        return;
+      }
+      console.log(data.item);
+
+      setNewsData(data.item);
+    })();
+  }, []);
+
   return (
     <div>
       <h1>News</h1>
       <nav>
-        <Link to="news_card">카드형</Link>
-        <Link to="news_list">리스트형</Link>
+        <Link to="/news/card">카드형</Link>
+        <Link to="/news/list">리스트형</Link>
       </nav>
       <Routes>
-        <Route path="news_card" element={<NewsCard news={newsData} />} />
-        <Route path="news_list" element={<NewsList news={newsData} />} />
+        <Route path="card" element={<NewsCard news={newsData} />} />
+        <Route path="list" element={<NewsList news={newsData} />} />
       </Routes>
     </div>
   );

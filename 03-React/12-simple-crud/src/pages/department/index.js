6173e194468655acfbaf29733d0eef467e0e765e
pongpongie <arevolvera@gmail.com>
2025-02-23 15:16:08 +0900

    [Feat] 12-simple-crud - step4

--- 03-React/12-simple-crud/src/pages/department/index.js
+++ 03-React/12-simple-crud/src/pages/department/index.js
@@ -7,6 +7,8 @@ import Table from "../../components/Table";
 import axiosHelper from "../../helpers/AxiosHelper";
 import utilHelper from "../../helpers/UtilHelper";
 
+import regexHelper from "../../helpers/RegexHelper";
+
 import { useNavigate, useLocation } from "react-router-dom";
 
 const DepartmentContainer = styled.div`
@@ -41,7 +43,7 @@ const Department = memo(() => {
 
       let data = null;
 
-      const args = {};
+      const args = { _sort: "id", _order: "desc" };
 
       if (keyword) {
         args["dname_like"] = keyword;
@@ -73,10 +75,72 @@ const Department = memo(() => {
     navigate(`/department?keyword=${word}`);
   }, []);
 
+  const onDataAddSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      const form = e.currentTarget;
+
+      try {
+        regexHelper.value("#dname", "학과명을 입력하세요");
+        regexHelper.maxLength("#dname", 20, "학과명은 20자 이내로 입력하세요");
+        regexHelper.value("#loc", "위치를 입력하세요");
+        regexHelper.maxLength("#loc", 20, "위치는 20자 이내로 검색하세요");
+      } catch (e) {
+        alert(e.message);
+        e.element.focus();
+        return;
+      }
+
+      const dname = form.dname.value;
+      const loc = form.loc.value;
+
+      (async () => {
+        setLoading(true);
+        let data = null;
+        try {
+          data = await axiosHelper.post("/department", {
+            dname: dname,
+            loc: loc,
+          });
+
+          console.group("데이터 저장 결과");
+          console.log(data);
+          console.groupEnd();
+        } catch (e) {
+          console.error(e);
+          alert(e.message);
+          return;
+        } finally {
+          setLoading(false);
+        }
+
+        const newData = [data.item, ...department];
+        setDepartment(newData);
+      })();
+    },
+    [department]
+  );
   return (
     <DepartmentContainer>
       <h2>Department</h2>
       <Spinner loading={loading} />
+
+      <form className="form-container" onSubmit={onDataAddSubmit}>
+        <input
+          type="text"
+          name="dname"
+          id="dname"
+          placeholder="학과명을 입력하세요"
+        />
+        <input
+          type="text"
+          name="loc"
+          id="loc"
+          placeholder="위치를 입력하세요"
+        />
+        <button type="submit">저장하기</button>
+      </form>
 
       <form className="form-container" onSubmit={onSearchSubmit}>
         <input type="text" name="keyword" defaultValue={keyword} />

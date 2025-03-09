385274fd6530c667eb82156e6cd4dceec8874aff
pongpongie <arevolvera@gmail.com>
2025-03-09 16:22:17 +0900

    [Feat] 16-hello-next - fix _app.js

--- 04-Next/16-hello-next/src/pages/_app.js
+++ 04-Next/16-hello-next/src/pages/_app.js
@@ -1,8 +1,9 @@
-// body 태그에서 모든 페이지에 공통으로 적용될 레이아웃을 구성하는 파일
-// props로 Compoentnt와 pageProps를 받아서 Component를 렌더링
-
+/**
+ * <body> 태그 영역 안에서 모든 페이지에 공통으로 적용될 레이아웃을 구성하는 파일
+ * props로 Component와 pageProps를 받아서 렌더링한다.
+ */
 import Meta from "@/components/Meta";
-import GlobalStyles from "@/components/GlobalStyles";
+import GlobalStyle from "@/components/GlobalStyles";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
@@ -10,11 +11,14 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Meta />
-      <GlobalStyles />
+      <GlobalStyle />
       <Header />
 
       <Component {...pageProps} />
+
       <Footer />
     </>
   );
 };
+
+export default App;

cb8bb70b820d122a82edda4a5363cbb171a45fd7
pongpongie <arevolvera@gmail.com>
2025-03-09 15:31:54 +0900

    [Feat] 16-hello-next - mod MenuLink, Meta according to Next.js

--- 04-Next/16-hello-next/src/components/MenuLink.js
+++ 04-Next/16-hello-next/src/components/MenuLink.js
@@ -1,44 +1,46 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
+import Link from "next/link";
 
 /** 메뉴링크 --> NavLink: 현재 머물고 있는 페이지와 관련된 링크에 CSS적용 */
-const MenuLinkContainer = styled(NavLink)`
-    font-size: 20px;
-    cursor: pointer;
-    text-decoration: none;
-    padding-bottom: 2px;
-    color: #222;
+const MenuLinkContainer = styled(Link)`
+  font-size: 20px;
+  cursor: pointer;
+  text-decoration: none;
+  padding-bottom: 2px;
+  color: #222;
 
-    /* CSS의 가상클래스 hover */
-    &:hover {
-        color: #22b8cf;
-    }
+  /* CSS의 가상클래스 hover */
+  &:hover {
+    color: #22b8cf;
+  }
 
+  &:after {
+    content: "|";
+    display: inline-block;
+    padding: 0 7px;
+    color: #ccc;
+  }
+
+  &:last-child {
     &:after {
-        content: '|';
-        display: inline-block;
-        padding: 0 7px;
-        color: #ccc;
+      /* 글자색을 흰색으로 지정하여 화면에서 숨긴다. */
+      color: #fff;
     }
+  }
 
-    &:last-child {
-        &:after {
-            /* 글자색을 흰색으로 지정하여 화면에서 숨긴다. */
-            color: #fff;
-        }
-    }
-
-    /*
+  /*
         URL이 현재 메뉴를 가르키는 경우 (콜론이 아닌 점에 주의)
         활성 메뉴에 적용되는 기본 클래스 이름이 'active'이다.
     */
-    &.active {
-        text-decoration: underline;
-        color: #22b8cf;
-    }
+  &.active {
+    text-decoration: underline;
+    color: #22b8cf;
+  }
 `;
 
-const MenuLink = ({to, children}) => <MenuLinkContainer to={to}>{children}</MenuLinkContainer>;
+const MenuLink = ({ to, children }) => (
+  <MenuLinkContainer to={to}>{children}</MenuLinkContainer>
+);
 
 export default MenuLink;
